245bbc82eabfcc3dc076dbb3422baf97d113b11d
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2017-09-15 01:54:03 -0700

    first commit

---
+++ src/config.jsx
@@ -0,0 +1,7 @@
+const API = __DEV__ ? 'http://localhost:3000/api/v1/videos' : '/api/v1/videos';
+const API_KEY = 'A3rDNhEsnR7m2UFeRSIchz';
+
+export {
+  API,
+  API_KEY,
+};
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import { render } from 'react-dom';
+import Routes from './routes';
+import '../dist/css/style.css';
+
+console.log(Routes);
+render(Routes, document.getElementById('app'));
